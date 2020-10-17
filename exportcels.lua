805529648ae33742edd6a6b81d7cbb4c931e3565
DevOrenge <samuel.orwoll@gmail.com>
2020-10-17 17:39:14 +0200

    small update

--- exportcels.lua
+++ exportcels.lua
@@ -3,9 +3,10 @@ local xmax = 0
 local ymax = 0
 local xmin = 100000
 local ymin = 100000
-
+local processedImgs = 0
 for key,value in ipairs(cels)
 do
+	processedImgs += 1
 	if(value.bounds.width + value.position.x > xmax)
 	then
 		xmax = value.bounds.width + value.position.x
@@ -25,16 +26,42 @@ do
 end
 
 local img = Image(xmax - xmin, ymax - ymin)
+local lastIndex = 0
+local currentLowestIndex = 100000
+
+while (processedImgs > 0)
+do
+
+currentLowestIndex = 100000
+
 for key,value in ipairs(cels)
 do
-	pos = Point(value.position.x - xmin, value.position.y - ymin)
-	for it in value.image:pixels() do
-		pixelValue = it()
-		alpha = app.pixelColor.rgbaA(pixelValue)
-		if alpha > 0 then
-			img:drawPixel(pos.x + it.x, pos.y + it.y, pixelValue)
+	layerIndex = value.layer.stackIndex
+	if(layerIndex > lastIndex && layerIndex < currentLowestIndex)
+	then
+		currentLowestIndex = layerIndex
+	end
+end
+
+lastIndex = currentLowestIndex
+processedImgs -= 1
+
+for key,value in ipairs(cels)
+do
+	layerIndex = value.layer.stackIndex
+	if(layerIndex == currentLowestIndex) 
+	then
+		pos = Point(value.position.x - xmin, value.position.y - ymin)
+		for it in value.image:pixels() do
+			pixelValue = it()
+			alpha = app.pixelColor.rgbaA(pixelValue)
+			if alpha > 0 then
+				img:drawPixel(pos.x + it.x, pos.y + it.y, pixelValue)
+			end
 		end
 	end
+end
+
 end
 
 local dlg = Dialog("Save selected cels")
